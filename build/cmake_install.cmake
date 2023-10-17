7bde5d72be232f987e8aa60b707c8e92c0b0db68
Eli Wilson <ewilson@vacuumtechnology.com>
2023-10-17 08:10:40 -0400

    cmake changes

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/ewilson/source/repos/FeatureDetection/source
+# Install script for directory: C:/Users/ewilson/source/repos/VTI-D541-Windows/source
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/FeatureDetection")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/FeatureDetection")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ewilson/source/repos/FeatureDetection/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ewilson/source/repos/VTI-D541-Windows/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
